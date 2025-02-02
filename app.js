3427c97cbbbe767c8770a02a2760f91386f255db
JGarcia575 <jimenagarcia575@gmail.com>
2025-02-02 19:08:59 -0300

    añadida la función agregarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//array que contiene los nombres de los amigos
+let nombresAmigos = [];
+
+
+function agregarAmigo() {
+    let nombre = document.getElementById('amigo').value;
+    console.log(nombre);
+
+    if (nombre === '') {
+        alert('Por favor, ingrese un nombre válido.');
+
+    } else {
+        nombresAmigos.push(nombre);
+        limpiarTexto();       
+    }; 
+    
+    return nombresAmigos;
+};
+
+function limpiarTexto() {
+    document.getElementById('amigo').value = '';
+}
+
+console.log('Hola, estoy funcionando');
